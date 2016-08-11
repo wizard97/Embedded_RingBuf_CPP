5fdc09f30c911eae0310284e635fe647efb8eaaf
Aaron Wisner <daw268@cornell.edu>
2016-08-11 13:17:59 +0100

    Moved implementation to cpp file

--- RingBuf.cpp
+++ RingBuf.cpp
@@ -1,1 +1,70 @@
 #include "RingBuf.h"
+
+template <typename Type, uint16_t MaxElements>
+RingBuf<Type, MaxElements>::RingBuf()
+: _buf{ 0 }
+{
+    _numElements = 0;
+
+    _head = 0;
+}
+
+template <typename Type, uint16_t MaxElements>
+bool RingBuf<Type, MaxElements>::add(Type &obj)
+{
+    if (isFull())
+        return false;
+
+    _buf[_head] = obj;
+    _head = (_head + 1)%MaxElements;
+    numElements++;
+    return true;
+}
+
+template <typename Type, uint16_t MaxElements>
+bool RingBuf<Type, MaxElements>::pull(Type *dest)
+{
+    uint16_t tail;
+
+    if (isEmpty())
+        return false;
+
+    tail = getTail();
+
+    *dest = _buf[tail];
+    numElements--;
+    return true;
+}
+
+template <typename Type, uint16_t MaxElements>
+bool RingBuf<Type, MaxElements>::isFull()
+{
+    return numElements >= MaxElements;
+}
+
+template <typename Type, uint16_t MaxElements>
+uint16_t RingBuf<Type, MaxElements>::numElements()
+{
+    return numElements;
+}
+
+template <typename Type, uint16_t MaxElements>
+bool RingBuf<Type, MaxElements>::isEmpty()
+{
+    return !numElements;
+}
+
+template <typename Type, uint16_t MaxElements>
+Type *RingBuf<Type, MaxElements>::peek(uint16_t index)
+{
+    if (index >= numElements)
+        return NULL;
+
+    return &_buf[(getTail() + index)%MaxElements];
+}
+
+template <typename Type, uint16_t MaxElements>
+uint16_t RingBuf<Type, MaxElements>::getTail()
+{
+    return (_head + (MaxElements - _numElements))%MaxElements;
+}
--- RingBuf.h
+++ RingBuf.h
@@ -3,80 +3,31 @@
 
 #include <Arduino.h>
 
-template<typename Type, uint16_t MaxElements>
+template <typename Type, uint16_t MaxElements>
 
 class RingBuf
 {
 public:
 
-RingBuf()
-: _buf{ 0 }
-{
-    _numElements = 0;
+RingBuf();
 
-    _head = 0;
-}
+bool add(Type &obj);
 
+bool pull(Type *dest);
 
-bool add(Type &obj)
-{
-    if (isFull())
-        return false;
+bool isFull();
 
-    _buf[_head] = obj;
-    _head = (_head + 1)%MaxElements;
-    numElements++;
-    return true;
-}
+uint16_t numElements();
 
-bool pull(Type *dest)
-{
-    uint16_t tail;
+bool isEmpty();
 
-    if (isEmpty())
-        return false;
-
-    tail = getTail();
-
-    *dest = _buf[tail];
-    numElements--;
-    return true;
-}
-
-bool isFull()
-{
-    return numElements >= MaxElements;
-}
-
-
-uint16_t numElements()
-{
-    return numElements;
-}
-
-
-bool isEmpty()
-{
-    return !numElements;
-}
-
-
-Type *peek(uint16_t index)
-{
-    if (index >= numElements)
-        return NULL;
-
-    return &_buf[(getTail() + index)%MaxElements];
-}
+Type *peek(uint16_t index);
 
 protected:
-uint16_t getTail()
-{
-    return (_head + (MaxElements - numElements))%MaxElements;
-}
+uint16_t getTail();
 
 Type _buf[MaxElements];
-uint16_t _head, _tail;
+uint16_t _head;
 
 uint16_t _numElements;
 private:
