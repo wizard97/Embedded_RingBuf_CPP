b6a1f3102f78942aa52ce3e4ecfcf7e3194e2507
Gonzalo Brusco <gonzabrusco@gmail.com>
2022-01-05 14:54:48 -0300

    Added Array Functions

--- RingBufCPP.h
+++ RingBufCPP.h
@@ -45,6 +45,48 @@ bool add(const Type &obj, bool overwrite=false)
     return !full;
 }
 
+/**
+* Adds "length" elements from the "obj" array to the buffer.
+*
+* If there is already MaxElements in the buffer,
+* the oldest element will either be overwritten (when overwrite is true) or
+* this add will have no effect (when overwrite is false).
+*
+* Return: number of elements actually added to the buffer (could be less than length if overwrite was set to false and MaxElements-_numElements < length)
+*/
+size_t add(const Type *obj, size_t length, bool overwrite=false)
+{
+    size_t ret = 0;
+
+    RB_ATOMIC_START
+    {
+        if((length > 0) && (length <= MaxElements)) { // Can't write more elements than the buffer size or zero elements.
+            if(!overwrite) {
+                // If overwrite is disabled, length should be trunked to the available space for writing in the buffer.
+                length = (MaxElements-_numElements) > length ? length : (MaxElements-_numElements);
+            }
+
+            if(length > 0) { // Check is there's space left for writing after trunking length.
+                if(length > (MaxElements-_head)) {
+                    // Needs two memcpy.
+                    memcpy(&_buf[_head],obj,sizeof(Type)*(MaxElements-_head)); // From _head to the end of _buf
+                    memcpy(_buf,&obj[MaxElements-_head],sizeof(Type)*(length-(MaxElements-_head))); // From the start of _buf until length-(MaxElements-_head)
+                }
+                else {
+                    // Only one copy is enough
+                    memcpy(&_buf[_head],obj,sizeof(Type)*length); // From _head to _head+length
+                }
+
+                _head = (_head+length) % MaxElements;
+                _numElements = (_numElements+length > MaxElements) ? MaxElements : _numElements+length;
+                ret = length;
+            }
+        }
+    }
+    RB_ATOMIC_END
+
+    return ret;
+}
 
 /**
 * Remove last element from buffer, and copy it to dest
@@ -70,6 +112,39 @@ bool pull(Type *dest)
     return ret;
 }
 
+/**
+* Remove "length" last elements from buffer, and copy them to "dest" buffer
+* Return: number of elements actually copied (could be less if numElements < length).
+*/
+size_t pull(Type *dest, size_t length)
+{
+    size_t tail;
+    size_t ret = 0;
+
+    RB_ATOMIC_START
+    {
+        if ((!isEmpty()) && (length > 0)) {
+            length = _numElements > length ? length : _numElements; // Trunk length if exceeds elements available for reading.
+
+            tail = getTail();
+            if(length > (MaxElements-tail)) {
+                // Needs two memcpy.
+                memcpy(dest,&_buf[tail],sizeof(Type)*(MaxElements-tail)); // From tail to the end of _buf
+                memcpy(&dest[MaxElements-tail],_buf,sizeof(Type)*(length-(MaxElements-tail))); // From the start of _buf until length-(MaxElements-tail)
+            }
+            else {
+                // Only one copy is enough
+                memcpy(dest,&_buf[tail],sizeof(Type)*length); // From tail to tail+length
+            }
+
+            _numElements = _numElements-length;
+            ret = length;
+        }
+    }
+    RB_ATOMIC_END
+
+    return ret;
+}
 
 /**
 * Peek at num'th element in the buffer
